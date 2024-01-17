141b424d5064de158f5258db90d0d2a1084c6e02
Abishek Thapa <abhishekthapa358@gmail.com>
2024-01-17 11:33:04 +0545

    File  updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors:{
-        "primary":"#141414",
-        "blue":"#0047AB"
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
