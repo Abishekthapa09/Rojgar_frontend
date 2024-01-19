dbbf806c59cb32f92313ece96fea9f91042fd7f8
Abishek Thapa <abhishekthapa358@gmail.com>
2024-01-19 13:23:50 +0545

    Footer section added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,77 +1,55 @@
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
 import { footerLinks } from "../utils/data";
 import { Link } from "react-router-dom";
 import TextInput from "./TextInput";
 import CustomButton from "./CustomButton";
+import { Facebook } from "../assets";
 
 const Footer = () => {
   return (
-    <footer className='text-white mp-20'>
-      <div className='bg-tertiary'>
-        <div className='container px-5 py-20 mx-auto '>
-          <div className='w-full flex flex-wrap gap-10 justify-between -mb-10 -px-4'>
-            {footerLinks.map(({ id, title, links }) => (
-              <div className='w-auto px-4 ' key={id + title}>
-                <h2 className='font-medium text-white tracking-widest text-sm mb-3'>
-                  {title}
-                </h2>
-
-                <div className='mb-10 flex flex-col gap-3 '>
-                  {links.map((link, index) => (
-                    <Link
-                      key={link + index}
-                      to='/'
-                      className='text-gray-300 text-sm hover:text-white '
-                    >
-                      {link}
-                    </Link>
-                  ))}
-                </div>
-              </div>
-            ))}
-          </div>
+    <footer className="footer p-10 bg-tertiary text-white">
+      <aside className=" -translate-y-4">
+        <img src="/public/WhiteLogo.png" className="pl-8" alt="White Logo" width={240}></img>
+        <header class=" text-white font-bold mx-auto text-base">Follow Us On</header>
+        <div class="grid grid-flow-col gap-4 mx-auto">
+          <a href="https://www.facebook.com"><FaFacebook size={28}/></a>
+          <a href="https://www.twitter.com"><FaTwitter size={28}/></a>
+          <a href="https://www.instagram.com"><FaInstagram size={28}/></a>
         </div>
-
-        <div className=''>
-          <p className='container px-5 mx-auto text-white mt-2 '>
-            Subscribe to our Newsletter
-          </p>
-
-          <div className='container mx-auto px-5 pt-6 pb-8 flex flex-wrap items-center justify-between '>
-            <div className='w-full md:w-2/4 lg:w-1/3 h-16 flex items-center justify-center md:justify-start '>
-              <TextInput
-                styles='w-full flex-grow md:w-40 2xl:w-64 bg-gray-100 sm:mr-4 md-2'
-                type='email'
-                placeholder='Email Address'
-              />
-
-              <CustomButton
-                title='Subscribe'
-                containerStyles={
-                  "block bg-primary text-white px-5 py-2.5 text-md rounded hover:bg-blue-800 focus:potline-none flex-col items-center mt-2"
-                }
-              />
-            </div>
-
-            <span className='inline-flex lg:ml-auto lg:mt-0 mt-6 w-full justify-center md:justify-start md:w-auto'>
-              <a className='text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FaFacebookF />
-              </a>
-              <a className='ml-3 text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FaTwitter />
-              </a>
-              <a className='ml-3 text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FiInstagram />
-              </a>
-
-              <a className='ml-3 text-white text-xl  hover:scale-125 ease-in-out duration-300'>
-                <FaLinkedinIn />
-              </a>
-            </span>
-          </div>
-        </div>
-      </div>
+        <p className=" font-mono text-sm pl-6">2024.All right reserved by Rojgar.com</p>
+      </aside>
+      <nav>
+        <header className="footer-title">Company</header>
+        <a href="" className="link link-hover">About Us</a>
+        <a href="" className="link link-hover">Terms of Use</a>
+        <a href="" className="link link-hover">Contact Us</a>
+        <a href="" className="link link-hover">Security Advice</a>
+      </nav>
+      <nav>
+        <header className="footer-title">Jobs By Location</header>
+        <a className="link link-hover">Jobs in Pokhara</a>
+        <a className="link link-hover">Jobs in Kathmandu</a>
+        <a className="link link-hover">Jobs in Butwal</a>
+        <a className="link link-hover">Jobs in Chitwan</a>
+        <a className="link link-hover">All location</a>
+      </nav>
+      <nav>
+        <header className="footer-title">Jobs By Function</header>
+        <a className="link link-hover">IT Software Jobs</a>
+        <a className="link link-hover">Sales Jobs</a>
+        <a className="link link-hover">Bank Jobs</a>
+        <a className="link link-hover">Freshers Job</a>
+        <a className="link link-hover">All Functional Areas</a>
+      </nav>
+      <nav>
+        <header className="footer-title">Jobs By Role</header>
+        <a className="link link-hover">Developer Jobs</a>
+        <a className="link link-hover">Accountant Jobs</a>
+        <a className="link link-hover">Branch Manager Role</a>
+        <a className="link link-hover">Chief Marketing Officer Jobs</a>
+      </nav>
+   
     </footer>
   );
 };
